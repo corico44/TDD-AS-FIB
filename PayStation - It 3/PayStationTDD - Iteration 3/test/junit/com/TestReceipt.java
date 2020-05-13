1bacdb70de21ea184e3a6cf62419f16e7e6a9085
Cori <cairigaray999@gmail.com>
2020-05-13 17:07:56 +0200

    Iteració 9

---
+++ PayStation - It 3/PayStationTDD - Iteration 3/test/junit/com/TestReceipt.java
@@ -0,0 +1,44 @@
+package junit.com;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/** Test cases for the Pay Station system.
+ 
+   This source code is from the book 
+     "Flexible, Reliable Software:
+       Using Patterns and Agile Development"
+     published 2010 by CRC Press.
+   Author: 
+     Henrik B Christensen 
+     Department of Computer Science
+     Aarhus University
+   
+   Please visit http://www.baerbak.com/ for further information.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+ 
+       http://www.apache.org/licenses/LICENSE-2.0
+ 
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+public class TestReceipt {
+  Receipt ps;
+  /** Fixture for pay station testing. */
+  
+  @Test
+  public void receiptCanStoreValues() {
+	    ps = new ReceiptImpl(20);
+	    assertEquals( "Receipt value is 20", 
+	                  20, ps.value() ); 
+	  }
+  
+
+}
